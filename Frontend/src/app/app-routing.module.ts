753f8617490a9f7e2c4a71c81d837019a3175a2e
balajidontul <balajidontulofficial@outlook.com>
2021-08-10 11:09:34 +0530

    Added Routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { NGOComponent } from './ngo/ngo.component';
+import { StepComponent } from './step/step.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'steppath', component:StepComponent },
+  {path:'', component:HomeComponent },
+  {path:'ngopath', component:NGOComponent },
+  {path:'adminpath', component:AdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
