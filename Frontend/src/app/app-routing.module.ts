81bcd5e1ef0fb2669949c329ecd029817e9efe50
balajidontul <balajidontulofficial@outlook.com>
2021-08-11 11:43:40 +0530

    Packge

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { CriteriaComponent } from './ngo/criteria/criteria.component';
+import { FundingnormsComponent } from './ngo/fundingnorms/fundingnorms.component';
 import { NGOComponent } from './ngo/ngo.component';
+import { OrganisationComponent } from './ngo/organisation/organisation.component';
+import { RegisterComponent } from './register/register.component';
 import { StepComponent } from './step/step.component';
 
 const routes: Routes = [
@@ -11,7 +14,10 @@ const routes: Routes = [
   {path:'', component:HomeComponent },
   {path:'ngopath', component:NGOComponent },
   {path:'adminpath', component:AdminComponent },
-  {path : 'criteriapath', component : CriteriaComponent}
+  {path : 'criteriapath', component : CriteriaComponent ,outlet: 'aux'},
+  {path : 'fundingnorms', component : FundingnormsComponent},
+  {path : 'organisation', component : OrganisationComponent},
+  {path : 'registerngo', component : RegisterComponent}
   
 ];
 
