44acde680ac65375eb6496600eab6ca6e0db9ec4
balajidontul <balajidontulofficial@outlook.com>
2021-08-11 12:00:34 +0530

    Modified

--- Backend/src/main/java/com/team6/model/NgoProject.java
+++ Backend/src/main/java/com/team6/model/NgoProject.java
@@ -1,5 +1,44 @@
 package com.team6.model;
 
 public class NgoProject {
+	
+	private int ngoProjectID;
+	private ProjectInCharge projectInCharge;
+	private OrganisationsInfo organisationsInfo;
+	public int getNgoProjectID() {
+		return ngoProjectID;
+	}
+	public void setNgoProjectID(int ngoProjectID) {
+		this.ngoProjectID = ngoProjectID;
+	}
+	public ProjectInCharge getProjectInCharge() {
+		return projectInCharge;
+	}
+	public void setProjectInCharge(ProjectInCharge projectInCharge) {
+		this.projectInCharge = projectInCharge;
+	}
+	public OrganisationsInfo getOrganisationsInfo() {
+		return organisationsInfo;
+	}
+	public void setOrganisationsInfo(OrganisationsInfo organisationsInfo) {
+		this.organisationsInfo = organisationsInfo;
+	}
+	public NgoProject(int ngoProjectID, ProjectInCharge projectInCharge, OrganisationsInfo organisationsInfo) {
+		super();
+		this.ngoProjectID = ngoProjectID;
+		this.projectInCharge = projectInCharge;
+		this.organisationsInfo = organisationsInfo;
+	}
+	public NgoProject() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "NgoProject [ngoProjectID=" + ngoProjectID + ", projectInCharge=" + projectInCharge
+				+ ", organisationsInfo=" + organisationsInfo + "]";
+	}
+	
+	
 
 }
--- Backend/src/main/java/com/team6/model/ProjectInCharge.java
+++ Backend/src/main/java/com/team6/model/ProjectInCharge.java
@@ -1,5 +1,72 @@
 package com.team6.model;
 
+
 public class ProjectInCharge {
 
+	
+	private int projectID;
+	private int organisationID;
+	private String projectName;
+	private String startDate;
+	private String endDate;
+	private String projectMentor;
+	public int getProjectID() {
+		return projectID;
+	}
+	public void setProjectID(int projectID) {
+		this.projectID = projectID;
+	}
+	public int getOrganisationID() {
+		return organisationID;
+	}
+	public void setOrganisationID(int organisationID) {
+		this.organisationID = organisationID;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getProjectMentor() {
+		return projectMentor;
+	}
+	public void setProjectMentor(String projectMentor) {
+		this.projectMentor = projectMentor;
+	}
+	public ProjectInCharge(int projectID, int organisationID, String projectName, String startDate, String endDate,
+			String projectMentor) {
+		super();
+		this.projectID = projectID;
+		this.organisationID = organisationID;
+		this.projectName = projectName;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.projectMentor = projectMentor;
+	}
+	public ProjectInCharge() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "ProjectInCharge [projectID=" + projectID + ", organisationID=" + organisationID + ", projectName="
+				+ projectName + ", startDate=" + startDate + ", endDate=" + endDate + ", projectMentor=" + projectMentor
+				+ "]";
+	}
+	
+	
+	
 }
