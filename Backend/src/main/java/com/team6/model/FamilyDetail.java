69321cfc6be56d6f95639535d494aa0aef747fb6
ramya <ramyaf976@gmail.com>
2021-08-11 11:50:07 +0530

    pojo commit

--- Backend/src/main/java/com/team6/model/FamilyDetail.java
+++ Backend/src/main/java/com/team6/model/FamilyDetail.java
@@ -1,5 +1,59 @@
 package com.team6.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="FamilyDetails")
 public class FamilyDetail {
+	
+	@Id
+	int regid;
+	String firstname;
+	String lastname;
+	String relationship;
+	public FamilyDetail() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public FamilyDetail(int regid, String firstname, String lastname, String relationship) {
+		super();
+		this.regid = regid;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.relationship = relationship;
+	}
+	public int getRegid() {
+		return regid;
+	}
+	public void setRegid(int regid) {
+		this.regid = regid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getRelationship() {
+		return relationship;
+	}
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+	@Override
+	public String toString() {
+		return "FamilyDetail [regid=" + regid + ", firstname=" + firstname + ", lastname=" + lastname
+				+ ", relationship=" + relationship + "]";
+	}
+	
+	
 
 }
