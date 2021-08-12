7a9d40ea23a207868380342f8be60435ac19220e
balajidontul <balajidontulofficial@outlook.com>
2021-08-12 15:50:31 +0530

    Organisation Integration with Backend

--- Backend/src/main/java/com/team6/model/PersonalDetail.java
+++ Backend/src/main/java/com/team6/model/PersonalDetail.java
@@ -17,67 +17,43 @@ public class PersonalDetail {
 	private String pan;
 	private int phone;
 	private String email;
-	
-	private UserRegistration userRegistration;
-
 	public int getRegId() {
 		return regId;
 	}
-
 	public void setRegId(int regId) {
 		this.regId = regId;
 	}
-
 	public String getMaritalStatus() {
 		return maritalStatus;
 	}
-
 	public void setMaritalStatus(String maritalStatus) {
 		this.maritalStatus = maritalStatus;
 	}
-
 	public String getAadharNo() {
 		return aadharNo;
 	}
-
 	public void setAadharNo(String aadharNo) {
 		this.aadharNo = aadharNo;
 	}
-
 	public String getPan() {
 		return pan;
 	}
-
 	public void setPan(String pan) {
 		this.pan = pan;
 	}
-
 	public int getPhone() {
 		return phone;
 	}
-
 	public void setPhone(int phone) {
 		this.phone = phone;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public UserRegistration getUserRegistration() {
-		return userRegistration;
-	}
-
-	public void setUserRegistration(UserRegistration userRegistration) {
-		this.userRegistration = userRegistration;
-	}
-
-	public PersonalDetail(int regId, String maritalStatus, String aadharNo, String pan, int phone, String email,
-			UserRegistration userRegistration) {
+	public PersonalDetail(int regId, String maritalStatus, String aadharNo, String pan, int phone, String email) {
 		super();
 		this.regId = regId;
 		this.maritalStatus = maritalStatus;
@@ -85,21 +61,14 @@ public class PersonalDetail {
 		this.pan = pan;
 		this.phone = phone;
 		this.email = email;
-		this.userRegistration = userRegistration;
 	}
-
 	public PersonalDetail() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public String toString() {
-		return "PersonalDetail [regId=" + regId + ", maritalStatus=" + maritalStatus + ", aadharNo=" + aadharNo
-				+ ", pan=" + pan + ", phone=" + phone + ", email=" + email + ", userRegistration=" + userRegistration
-				+ "]";
-	}
 	
+
+
 	
 	
 	
