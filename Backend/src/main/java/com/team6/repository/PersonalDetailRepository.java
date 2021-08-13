2c301457cd28013c74bb7414b15eebc52e163400
Nishawadhwani14 <60443135+Nishawadhwani14@users.noreply.github.com>
2021-08-13 00:46:26 -0700

    Delete PersonalDetailRepository.java

--- Backend/src/main/java/com/team6/repository/PersonalDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team6.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.team6.model.PersonalDetail;
-
-@Repository
-public interface PersonalDetailRepository extends JpaRepository<PersonalDetail, Integer>{
-
-}
