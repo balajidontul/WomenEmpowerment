18080770e87b247ceb91c5be2ab01e7e99ad3871
Nishawadhwani14 <60443135+Nishawadhwani14@users.noreply.github.com>
2021-08-13 02:23:19 -0700

    Update PersonalDetailController.java

--- Backend/src/main/java/com/team6/controller/PersonalDetailController.java
+++ Backend/src/main/java/com/team6/controller/PersonalDetailController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.team6.model.PersonalDetail;
 import com.team6.repository.PersonalDetailRepository;
+import com.team6.service.PersonalDetailService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -23,14 +25,17 @@ public class PersonalDetailController {
 		@Autowired
 		PersonalDetailRepository personalRepo;
 		
+		@Autowired
+		PersonalDetailService personalDetailService;
+		
 		@GetMapping("/personaldetail")
 		public List<PersonalDetail> getAll() {
 			return personalRepo.findAll();
 		}
 		
-		@PostMapping("/personaldetail")
-		public PersonalDetail addCourse(@RequestBody PersonalDetail personalDetail) {
-			return personalRepo.save(personalDetail);
+		@PostMapping("/personaldetail/{user_id}" )
+		public boolean addCourse(@RequestBody PersonalDetail personalDetail, @PathVariable("user_id") int user_id) {
+			return personalDetailService.saveRecords(personalDetail, user_id);
 		}
 		
 		@DeleteMapping("/personaldetail")
@@ -40,13 +45,13 @@ public class PersonalDetailController {
 			
 		}
 		
-		@PutMapping("/personaldetail")
-		public String updateProduct(@RequestBody PersonalDetail personalDetail) {
-			PersonalDetail detailsUpd = personalRepo.findById(personalDetail.getRegId()).get();
-			detailsUpd = personalDetail;
-			personalRepo.save(detailsUpd);
-			return "updated";
-		}
+//		@PutMapping("/personaldetail")
+//		public String updateProduct(@RequestBody PersonalDetail personalDetail) {
+//			PersonalDetail detailsUpd = personalRepo.findById(personalDetail.getRegId()).get();
+//			detailsUpd = personalDetail;
+//			personalRepo.save(detailsUpd);
+//			return "updated";
+//		}
 	}
 
 
