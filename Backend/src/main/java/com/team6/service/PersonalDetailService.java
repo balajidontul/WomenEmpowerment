ac832e235b7c4ad0cb69fe481298bc6caedc6278
Nishawadhwani14 <60443135+Nishawadhwani14@users.noreply.github.com>
2021-08-13 02:23:58 -0700

    Update PersonalDetailService.java

--- Backend/src/main/java/com/team6/service/PersonalDetailService.java
+++ Backend/src/main/java/com/team6/service/PersonalDetailService.java
@@ -1,8 +1,29 @@
 package com.team6.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.team6.model.PersonalDetail;
+import com.team6.model.UserRegistration;
+import com.team6.repository.PersonalDetailRepository;
+import com.team6.repository.UserRegistrationRepository;
 
 @Service
 public class PersonalDetailService {
+
+	@Autowired
+	PersonalDetailRepository personalDetailRepository;
+	
+	@Autowired
+	UserRegistrationRepository userRegistrationRepository;
+	
+	public boolean saveRecords(PersonalDetail personalDetail, int user_id) {
+		UserRegistration user = userRegistrationRepository.findById(user_id).get();
+		personalDetail.setUserRegistration(user);
+		personalDetailRepository.save(personalDetail);
+		return true;
+	}
+	
+	
 
 }
