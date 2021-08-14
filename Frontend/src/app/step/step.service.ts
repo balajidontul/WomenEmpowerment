ae1be94f5c673c9d2dacc3c9d748d62615dd5344
ramya <ramyaf976@gmail.com>
2021-08-14 18:31:34 +0530

    done

--- Frontend/src/app/step/step.service.ts
+++ Frontend/src/app/step/step.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Personaldetails } from './personaldetails';
 import { Familydetails } from './familydetails';
 import { Coursedetails } from './coursedetails';
-import {NgoDetails} from './ngodetails';
+//import {NgoDetails} from './ngodetails';
 import {NgoCourseDetails} from './ngocoursedetails';
 
 
@@ -43,10 +43,10 @@ export class StepService {
     return this.httpClient.get<Coursedetails[]>(`${this.baseURL}courses`);
   }
 
-  getAllNgos(): Observable<NgoDetails[]> {
+/*  getAllNgos(): Observable<NgoDetails[]> {
     return this.httpClient.get<NgoDetails[]>(`${this.baseURL}Organisation`)
   }
-
+*/
   getAllNgocourses(): Observable<NgoCourseDetails[]> {
     return this.httpClient.get<NgoCourseDetails[]>(`${this.baseURL}ngo-courses`)
   }
