8662c6e351e92c5b95a5f2ca470241473594c9b8
NSTiwari <tiwarinitin1999@gmail.com>
2021-08-15 22:28:58 +0530

    Added graphical data on admin dashboard.

--- Frontend/src/app/adminhome/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/src/app/adminhome/admin-dashboard/admin-dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Ngo } from 'src/app/ngo/ngo';
 import { NgoService } from 'src/app/ngo/ngo.service';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -13,28 +16,44 @@ export class AdminDashboardComponent implements OnInit {
   ngos:Ngo[];
   noOfApprovedNgo:number;
   noOfPendingNgo:number;
-  constructor(private ngoService:NgoService, private router:Router) { }
+  doughnutChartLabels: Label[];
+  doughnutChartData: MultiDataSet = [[0,0]] ;
+  doughnutChartType: ChartType;
+  chartReady:boolean;
+  constructor(private ngoService:NgoService, private router:Router) {
+    
+   }
 
   ngOnInit(): void {
     this.getApprovedNgo();
     this.getPendingNgo();
+    this.showChart();
   }
 
-  private getApprovedNgo() {
+  public getApprovedNgo() {
     this.ngoService.getApprovedNgoList().subscribe(data => {
       this.ngos = data;
       this.noOfApprovedNgo = this.ngos.length;
-  
-
     })
   }
 
   
-  private getPendingNgo() {
+  public getPendingNgo() {
     this.ngoService.getPendingNgoList().subscribe(data => {
       this.ngos = data;
       this.noOfPendingNgo = this.ngos.length;
+    
     })
   }
 
-}
+  public async showChart() {
+    this.getPendingNgo();
+    this.getApprovedNgo();
+    await new Promise(f => setTimeout(f, 1000));
+    this.doughnutChartLabels = ['Approved NGO', 'Pending NGO'];
+    this.doughnutChartData = [[this.noOfApprovedNgo, this.noOfPendingNgo]];
+    this.doughnutChartType = 'doughnut';
+    this.chartReady=true;
+  }
+ }
+  
