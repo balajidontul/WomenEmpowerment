a0fc68f88048394f88fb0528046a420aa658eebe
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2021-08-15 18:29:42 +0530

    Update admin-dashboard.component.ts

--- Frontend/src/app/adminhome/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/src/app/adminhome/admin-dashboard/admin-dashboard.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  ngos:Ngo[];
+  noOfApprovedNgo:number;
+  noOfPendingNgo:number;
+  constructor(private ngoService:NgoService, private router:Router) { }
 
   ngOnInit(): void {
+    this.getApprovedNgo();
+    this.getPendingNgo();
+  }
+
+  private getApprovedNgo() {
+    this.ngoService.getApprovedNgoList().subscribe(data => {
+      this.ngos = data;
+      this.noOfApprovedNgo = this.ngos.length;
+  
+
+    })
+  }
+
+  
+  private getPendingNgo() {
+    this.ngoService.getPendingNgoList().subscribe(data => {
+      this.ngos = data;
+      this.noOfPendingNgo = this.ngos.length;
+    })
   }
 
 }
