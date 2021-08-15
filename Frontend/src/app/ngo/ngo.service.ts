6ca4dec93451b7cd3ac9480df301ebf71625a357
balajidontul <balajidontulofficial@outlook.com>
2021-08-15 20:47:17 +0530

    msg

--- Frontend/src/app/ngo/ngo.service.ts
+++ Frontend/src/app/ngo/ngo.service.ts
@@ -32,7 +32,7 @@ export class NgoService {
   }
 
   getallngo():Observable<Ngo[]>{
-    return this.httpClient.get<Ngo[]>(`${this.baseURL}Organisation`);
+    return this.httpClient.get<Ngo[]>(`http://localhost:9000/rest/api/Organisation`);
   }
 
 }
