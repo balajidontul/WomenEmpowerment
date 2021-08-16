b405e400b41d223910cc8084e6ad5c182b740715
s1ddarth-7 <88031213+s1ddarth-7@users.noreply.github.com>
2021-08-16 18:50:53 +0530

    Changes made in order to add org password

--- Frontend/src/app/ngo/registerngo/registerngo.component.ts
+++ Frontend/src/app/ngo/registerngo/registerngo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginComponent } from 'src/app/login/login.component';
 import { Ngo } from '../ngo';
 import { NgoService } from '../ngo.service';
 import { Project } from '../project';
@@ -13,6 +14,8 @@ export class RegisterngoComponent implements OnInit {
   OrganisationReg:Ngo
   OrgIdObj:Ngo
   ProjectDet: Project
+  orgkey:number
+  dummypass:string
 
   loc:String
 
@@ -27,20 +30,30 @@ export class RegisterngoComponent implements OnInit {
 
   formSubmit(regForm:any)
   {
-    alert("NGO sent for Admin approval, you can toggle to other pages now safely!");
+    
     this.OrganisationReg.status="Pending";
     //console.log("Form value: "+regForm.value);
-    console.log(this.OrganisationReg);
-    console.log(this.ProjectDet);
+    //console.log(this.OrganisationReg);
+    //console.log(this.ProjectDet);
     this.ngoService.addNgo(this.OrganisationReg).subscribe(
       (data)=>{
         console.log("Return Value from REST"+data);
+        this.OrganisationReg=data as Ngo;
+        this.orgkey=this.OrganisationReg.organisationId;
+        alert("Please note your Organisation ID: "+this.orgkey+"\nPlease use it for future login!")
+        this.ngoService.addProj(this.ProjectDet,this.orgkey).subscribe(
+          (data2) => {console.log("Proj data: "+data2)}
+        );
       }
     ) 
-    this.ngoService.addProj(this.ProjectDet,1).subscribe();
-  }
-
-  onRegister() {
+    //this.ngoService.addProj(this.ProjectDet,this.orgkey).subscribe();
+    alert("NGO sent for Admin approval!");
     this.router.navigate(['/ngologin']);
   }
+
+  login()
+  {
+    this.router.navigate(['/ngologin']);
+  }
+
 }
