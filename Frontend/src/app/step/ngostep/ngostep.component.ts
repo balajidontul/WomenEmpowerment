4f2c5996e5f11b6eb1ea8743949c10923ecb06e5
NSTiwari <tiwarinitin1999@gmail.com>
2021-08-16 12:38:36 +0530

    Show list of NGOs in STEP component.

--- Frontend/src/app/step/ngostep/ngostep.component.ts
+++ Frontend/src/app/step/ngostep/ngostep.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { StepService } from '../step.service';
 import { Router } from '@angular/router';
 import {NgoDetails} from '../ngodetails';
+import { NgoService } from 'src/app/ngo/ngo.service';
+import { Ngo } from 'src/app/ngo/ngo';
 
 
 @Component({
@@ -12,14 +14,18 @@ import {NgoDetails} from '../ngodetails';
 export class NgostepComponent implements OnInit {
 
   NgoNames: NgoDetails[];
-  constructor(private stepService : StepService, private router : Router) { }
+  ngos:Ngo[];
+  constructor(private stepService : StepService, private router : Router,
+    private ngoService:NgoService) { }
+
+  
 
   ngOnInit(): void {
     this.getAllNgos();
   }
 
   getAllNgos() {
-     this.stepService.getAllNgos().subscribe(data => {
+     this.ngoService.getApprovedNgoList().subscribe(data => {
       console.log(data);
       this.NgoNames = data;
     })
